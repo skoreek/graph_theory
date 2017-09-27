0ff7579d9c719e109b54f2851ddb6e5f43d6ef66
skoreek <skoreekov@outlook.com>
2017-09-27 23:43:14 +0500

    Graph Theory

---
+++ Graph Theory/Graph Theory/Main.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+class Graph
+{
+private:
+	vector<int> vecGraph;
+
+public:
+
+	void readGraph(string fileName)
+	{
+		ifstream read(fileName);
+		if (!read.is_open())
+			cout << "File not found!" << endl;
+
+		else
+			copy(istream_iterator<int>(read), istream_iterator<int>(), back_inserter(vecGraph));
+	}
+	void writeGraph(string fileName)
+	{
+		ofstream write(fileName);
+		if (!write.is_open())
+			cout << "File not found!" << endl;
+
+		else
+		{
+			copy(vecGraph.begin(), vecGraph.end(), ostream_iterator<int>(write, " "));
+			write.close();
+		}
+	}
+};
+
+int main()
+{
+
+	Graph myGraph;
+
+	myGraph.readGraph("input.txt");
+	myGraph.writeGraph("output.txt");
+
+	return 0;
+}
